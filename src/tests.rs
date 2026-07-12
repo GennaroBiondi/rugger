d31ae31eb3094339e58dbf1628a3ac6e55a71b12
GennaroBiondi <throwawayishere@ultrakill.com>
2026-07-12 10:17:47 +0200

    test: update tests

--- src/tests.rs
+++ src/tests.rs
@@ -5,7 +5,23 @@ fn test_logging() {
     let my_log_message: String = String::from("this is a log message!");
     let my_log_level: LogLevel = LogLevel::Info;
 
-    let log_message = LogMessage::new(my_log_message, my_log_level);
+    let mut log_message = LogMessage::new(my_log_message, my_log_level);
+    assert_eq!(
+        log_message.to_string(),
+        "[INFO]: this is a log message!",
+        "Logs don't follow the specification"
+    );
+    log_message.log_level = LogLevel::Warning;
+    assert_eq!(
+        log_message.to_string(),
+        "[WARNING]: this is a log message!",
+        "Logs don't follow the specification"
+    );
 
-    println!("{}", log_message);
+    log_message.log_level = LogLevel::Error;
+    assert_eq!(
+        log_message.to_string(),
+        "[ERROR]: this is a log message!",
+        "Logs don't follow the specification"
+    );
 }
