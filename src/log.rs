95fe0ed8cb5f71e283e6b0d9134af780f62761f9
GennaroBiondi <throwawayishere@ultrakill.com>
2026-07-12 10:25:44 +0200

    feat(logging): make LogLevel flexible, so consumers can add their own Levels

--- src/log.rs
+++ src/log.rs
@@ -1,12 +1,14 @@
 use std::fmt::Display;
 
-pub enum LogLevel {
+pub trait LogLevel: Display {}
+
+pub enum StandardLogLevel {
     Info,
     Warning,
     Error,
 }
 
-impl Display for LogLevel {
+impl Display for StandardLogLevel {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Info => write!(f, "[INFO]"),
@@ -16,13 +18,15 @@ impl Display for LogLevel {
     }
 }
 
-pub struct LogMessage {
+impl LogLevel for StandardLogLevel {}
+
+pub struct LogMessage<T: LogLevel> {
     pub message: String,
-    pub log_level: LogLevel,
+    pub log_level: T,
 }
 
-impl LogMessage {
-    pub fn new(message: impl Into<String>, log_level: LogLevel) -> Self {
+impl<T: LogLevel> LogMessage<T> {
+    pub fn new(message: impl Into<String>, log_level: T) -> Self {
         LogMessage {
             message: message.into(),
             log_level,
@@ -30,7 +34,7 @@ impl LogMessage {
     }
 }
 
-impl Display for LogMessage {
+impl<T: LogLevel> Display for LogMessage<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}: {}", self.log_level, self.message)
     }
