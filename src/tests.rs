159fdbbc35919611a7741214a06fa2d5a0011e61
GennaroBiondi <throwawayishere@ultrakill.com>
2026-07-12 10:29:30 +0200

    test: add flexibility tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,11 @@
-use crate::{LogLevel, LogMessage};
+use std::fmt::Display;
+
+use crate::{LogLevel, LogMessage, StandardLogLevel};
 
 #[test]
 fn test_logging() {
     let my_log_message: String = String::from("this is a log message!");
-    let my_log_level: LogLevel = LogLevel::Info;
+    let my_log_level: StandardLogLevel = StandardLogLevel::Info;
 
     let mut log_message = LogMessage::new(my_log_message, my_log_level);
     assert_eq!(
@@ -11,17 +13,52 @@ fn test_logging() {
         "[INFO]: this is a log message!",
         "Logs don't follow the specification"
     );
-    log_message.log_level = LogLevel::Warning;
+    log_message.log_level = StandardLogLevel::Warning;
     assert_eq!(
         log_message.to_string(),
         "[WARNING]: this is a log message!",
         "Logs don't follow the specification"
     );
 
-    log_message.log_level = LogLevel::Error;
+    log_message.log_level = StandardLogLevel::Error;
     assert_eq!(
         log_message.to_string(),
         "[ERROR]: this is a log message!",
+        "Logs don't follow the specification"
+    );
+}
+
+#[test]
+fn test_flexibility() {
+    enum MyLogLevel {
+        Foo,
+        Bar,
+    }
+
+    impl Display for MyLogLevel {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                Self::Foo => write!(f, "[FOO]"),
+                Self::Bar => write!(f, "[BAR]"),
+            }
+        }
+    }
+
+    impl LogLevel for MyLogLevel {}
+
+    let my_log_message: String = String::from("this is a log message!");
+    let my_log_level = MyLogLevel::Bar;
+
+    let mut log_message = LogMessage::new(my_log_message, my_log_level);
+    assert_eq!(
+        log_message.to_string(),
+        "[BAR]: this is a log message!",
+        "Logs don't follow the specification"
+    );
+    log_message.log_level = MyLogLevel::Foo;
+    assert_eq!(
+        log_message.to_string(),
+        "[FOO]: this is a log message!",
         "Logs don't follow the specification"
     );
 }
